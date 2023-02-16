60adfff4c3a6985c46ec64d0d34d6326b5e9f6b1
Yash Patil <yashpatil.yp2711@gmail.com>
2023-02-16 09:34:48 +0530

    Initial server set-up done

---
+++ server.js
@@ -0,0 +1,28 @@
+// Import required modules
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+//create express app
+const app = express();
+dotenv.config();
+
+//setup middleware
+app.use(express.json());
+
+//connect to MongoDB
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    console.log('Connected to MongoDB...');
+})
+
+
+//import routes
+//TODO: remove this test route
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+//server listening
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
